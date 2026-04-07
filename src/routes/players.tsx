7b79f84905d56de4cd5691aae8cf7d073214000c
XavierBadayos <xavierbadayos@gmail.com>
2026-04-07 21:27:39 +0930

    add dummy cards and api

--- src/routes/players.tsx
+++ src/routes/players.tsx
@@ -1,9 +1,58 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/players')({
-  component: RouteComponent,
+  component: Players,
 })
 
-function RouteComponent() {
-  return <div>Hello "/players"!</div>
+function Players() {
+  return (
+    <div className='p-12 grid grid-cols-5 gap-4'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Kawhi Leonard</CardTitle>
+          <CardDescription>The Klaw</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Boardman gets paid.</p>
+      </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Kawhi Leonard</CardTitle>
+          <CardDescription>The Klaw</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Boardman gets paid.</p>
+      </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Kawhi Leonard</CardTitle>
+          <CardDescription>The Klaw</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Boardman gets paid.</p>
+      </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Kawhi Leonard</CardTitle>
+          <CardDescription>The Klaw</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Boardman gets paid.</p>
+      </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Kawhi Leonard</CardTitle>
+          <CardDescription>The Klaw</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Boardman gets paid.</p>
+      </CardContent>
+      </Card>
+    </div>
+  )
 }
