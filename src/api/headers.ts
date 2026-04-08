be9d776b03c66d0b7cae39b8d87d955d4fa56fd8
XavierBadayos <xavierbadayos@gmail.com>
2026-04-09 02:11:53 +0930

    separate headers from server function

---
+++ src/api/headers.ts
@@ -0,0 +1,11 @@
+export const playerStatsHeader = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Referer": "https://www.nba.com/",
+        "Origin": "https://www.nba.com",
+        "x-nba-stats-origin": "stats",
+        "x-nba-stats-token": "true",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "cors"
+    }
