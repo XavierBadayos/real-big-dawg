67768567bbf2e657ede6867ba95db1fb5061e592
XavierBadayos <xavierbadayos@gmail.com>
2026-04-09 02:12:12 +0930

    add tanstack query to index route

--- src/routes/players/index.tsx
+++ src/routes/players/index.tsx
@@ -1,20 +1,29 @@
 import { fetchAllPlayerStats } from '@/api/playerStatsApi'
 import { PlayerCardList } from '@/components/players/PlayerCardList';
 import { ModeToggle } from '@/components/theme/theme-toggle';
+import type { PlayerCardData } from '@/types/PlayerCard';
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router'
 
+const playerQuery = queryOptions({
+  queryKey: ['players'],
+  queryFn: () => fetchAllPlayerStats(),
+  staleTime: 1000 * 60 * 60,
+  gcTime: 1000 * 60 * 10,
+})
+
 export const Route = createFileRoute('/players/')({
+  loader: ({ context }) => context.queryClient.ensureQueryData(playerQuery),
   component: Players,
-  loader: async () => await fetchAllPlayerStats()
 })
 
 function Players() {
-  const data = Route.useLoaderData() ?? [];
-
+  const {data}  = useSuspenseQuery(playerQuery)
+  
   return (
     <div className='p-12 gap-4'>
       <ModeToggle />
-      <PlayerCardList data={data}/>
+      <PlayerCardList data={data as PlayerCardData[]}/>
     </div>
   )
 } 
