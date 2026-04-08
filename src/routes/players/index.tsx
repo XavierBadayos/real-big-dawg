7c700c9006624bc054678f65277c0478ced1f3d0
XavierBadayos <xavierbadayos@gmail.com>
2026-04-09 05:09:48 +0930

    increase stale time and gc time

--- src/routes/players/index.tsx
+++ src/routes/players/index.tsx
@@ -8,8 +8,8 @@ import { createFileRoute, Link } from '@tanstack/react-router'
 const playerQuery = queryOptions({
   queryKey: ['players'],
   queryFn: () => fetchAllPlayerStats(),
-  staleTime: 1000 * 60 * 60,
-  gcTime: 1000 * 60 * 60,
+  staleTime: 1000 * 60 * 60 * 4,
+  gcTime: 1000 * 60 * 60 * 24,
 })
 
 export const Route = createFileRoute('/players/')({
